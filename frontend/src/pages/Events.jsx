c85bd670e0d9915eddc359013e09e28739c77e24
Andrea Huang <huangandrea@hotmail.it>
2025-05-07 11:49:27 +0200

    add components and navigation

---
+++ frontend/src/pages/Events.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+const EVENTS = [
+  {
+    id: "e1",
+    title: "Event 1",
+  },
+  {
+    id: "e2",
+    title: "Event 2",
+  },
+  {
+    id: "e3",
+    title: "Event 3",
+  },
+];
+
+export default function EventsPage() {
+  return (
+    <>
+      <h1>The Events Page</h1>
+      <ul>
+        {EVENTS.map((event) => {
+            return <li key={event.id}>
+                <Link to={event.id} relative="path">{event.title}</Link>
+            </li>
+        })}
+      </ul>
+    </>
+  );
+}
