b733b4028c4c41c3c31ab5ba4e990d8068269255
Andrea Huang <huangandrea@hotmail.it>
2025-05-07 12:10:26 +0200

    Put the loader code inside the page for which it loads data

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,14 +1,22 @@
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData } from "react-router-dom";
 
-import EventsList from '../components/EventsList';
+import EventsList from "../components/EventsList";
 
-function EventsPage() {
-  // fetch the data from closest component
+export default function EventsPage() {
+  // fetch the data from closest component, any children of the component with
+  // the loader can access the data (e.g. access directly inside EventsList)
   const events = useLoaderData();
 
-  return (
-    <EventsList events={events} />
-  );
+  return <EventsList events={events} />;
 }
 
-export default EventsPage;
+export const loader = async () => {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // ...
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+};
