99b99c35dcda5d12d34efc83c4cff29fb1cfc645
Andrea Huang <huangandrea@hotmail.it>
2025-05-07 16:46:58 +0200

    The useRouteLoaderData() Hook & Accessing Data From Other Routes

--- frontend/src/pages/EventDetail.jsx
+++ frontend/src/pages/EventDetail.jsx
@@ -1,24 +1,26 @@
-import { useLoaderData, useParams } from "react-router-dom";
-import EventItem from "../components/EventItem"
+import { useRouteLoaderData } from "react-router-dom";
+import EventItem from "../components/EventItem";
 export default function EventDetailPage() {
-  const data = useLoaderData();
-  
-  return (
-    <EventItem event={data.event}/>
-  );
+  const data = useRouteLoaderData("event-detail");
+
+  return <EventItem event={data.event} />;
 }
 
-export async function loader({request, params}) {
+export async function loader({ request, params }) {
   const id = params.eventId;
 
-  const response = await fetch("http://localhost:8080/events/" + id)
+  const response = await fetch("http://localhost:8080/events/" + id);
 
-  if (!response.ok){
-    throw  new Response(JSON.stringify({ message: "Could not fetch details for selected event." }), {
-      status: 500,
-    })
+  if (!response.ok) {
+    throw new Response(
+      JSON.stringify({
+        message: "Could not fetch details for selected event.",
+      }),
+      {
+        status: 500,
+      }
+    );
   } else {
-    return response
+    return response;
   }
-  
 }
