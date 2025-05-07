e9f4ded32bd4bc3e1cb31fb3d168d9db24ef5bda
Andrea Huang <huangandrea@hotmail.it>
2025-05-07 22:08:07 +0200

    Deferring Data Fetching with defer() (postpone loading parts of the page because data has not arrived yet, very useful when lots of HTTP requests on same page with different speed)

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,22 +1,25 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, Await } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 export default function EventsPage() {
   // fetch the data from closest component, any children of the component with
   // the loader can access the data (e.g. access directly inside EventsList)
-  const data = useLoaderData();
-  const events = data.events;
+  const { events } = useLoaderData();
 
-  if (data.isError) {
-    return <p>{data.message}</p>;
-  }
-
-  return <EventsList events={events} />;
+  return (
+    // Suspense: show a fallback while waiting children data
+    // Await: wait for events Promise to be resolved
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading ...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
 }
 
-// loader functions can call browser API, but not hooks because we are not in a React component
-export const loader = async () => {
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
@@ -35,6 +38,18 @@ export const loader = async () => {
     // NOTE: loader accepts any kind of data, in particular it handles the Response
     // class that is normally returned by fetch() function, and it extracts and
     // returns the payload automatically for you
-    return response;
+    // return response;
+
+    // with defer, the payload is not extracted. Need to do it manually:
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+// loader functions can call browser API, but not hooks because we are not in a React component
+export const loader = async () => {
+  // NOTE: to defer, return an object with Promises
+  return {
+    events: loadEvents(), // events is a promise!
+  };
 };
